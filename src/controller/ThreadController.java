db2295b65b58f48c629735dd1bd7d5cc915f1309
Laboratorio101 <Laboratorio101@LAB102-01>
2020-02-28 16:29:11 -0300

    ex 2

--- src/controller/ThreadController.java
+++ src/controller/ThreadController.java
@@ -1,5 +1,50 @@
 package controller;
 
-public class ThreadController {
+import javax.swing.JOptionPane;
 
+public class ThreadController extends Thread{
+	int a,b,c;
+	public ThreadController(int a, int b, int c,int[] vt1,int[] vt2) {
+		this.a=a;
+		this.b=b;
+		this.c=c;
+		
+	}
+
+	@Override
+	public void run() {
+		escolhaTemp(a);
+		Temperatura(vt1);
+		
+	}
+
+	private void escolhaTemp(int op) {
+		op=(int)(Math.random()*3+1);
+		
+		switch(op) {
+		case 1:
+			convertK();
+			break;
+		case 2:
+			convertF();
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void convertC() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void convertF() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void convertK() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,10 +1,21 @@
 package view;
 
+import controller.ThreadController;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a,b,c;
+		int td;
+		int []vt1=new int[10];
+		int []vt2=new int[10];
+		
+		for(td=0;td<10;td++) {
+			Thread th=new ThreadController(a,b,c,vt1,vt2);
+				
+			th.start();
+			}
+		}
 	}
 
 }
